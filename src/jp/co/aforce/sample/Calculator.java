5b5af51ac1059d75ff8381fda07a4b1ff6cc33d6
SuzunoKatamura <suzuno.katamura@acro-net.co.jp>
2026-04-21 17:50:24 +0900

    演習問題の三章途中までの回答

--- src/jp/co/aforce/sample/Calculator.java
+++ src/jp/co/aforce/sample/Calculator.java
@@ -10,4 +10,24 @@ public class Calculator {
 		/*　計算した結果を表示する　*/
 		System.out.println("1 + 2 = " + c);
 	}
+	
+//	static void q1() {
+//		int num = 10;
+//	    System.out.println("num");
+//	  }
+//	
+//	static void q2() {
+//		int a = 8;
+//		int b = 5;
+//		int sum = a + b;
+//	    System.out.println("sum");
+//	  }
+//	
+//	static void q3() {
+//		int width = 7;
+//		int height = 4;
+//		int area = width * height / 2;
+//	    System.out.println("area");
+//	  }
 }
+
---
+++ src/jp/co/aforce/sample/practice1.java
@@ -0,0 +1,111 @@
+package jp.co.aforce.sample;
+
+import java.util.Scanner;
+
+public class practice1 {
+	
+	  public static void main(String[] args) {
+	    q1();
+	    q2();
+	    q3();
+	    q4();
+	    q5();
+	    q6();
+	    q7();
+	    q8();
+	    q9();
+	    q10();
+	    q11();
+	    q12();
+	    q13();
+	    q14();
+	  }
+	
+		  static void q1() {
+		    int num = 10;
+		    System.out.println(num);
+		  }
+		
+		  static void q2() {
+		    int a = 8;
+		    int b = 5;
+		    int sum = a + b;
+		    System.out.println(sum);
+		  }
+		
+		  static void q3() {
+		    int width = 7;
+		    int height = 4;
+		    int area = width * height / 2;
+		    System.out.println(area);
+		  }
+		  
+		  static void q4() {
+			    String name = "Java";
+			    double version = 17.0;
+			    boolean isFun = true;
+			    System.out.println(name + " " + version + " " + isFun);
+			  }
+		  
+		  static void q5() {
+			    Scanner scanner = new Scanner(System.in);
+
+			    System.out.print("名前を入力してください：");
+			    String name = scanner.nextLine();
+
+			    System.out.println("こんにちは, " + name + " さん！");
+
+			    scanner.close();
+			  }
+		  
+		  
+		  
+		  
+		  
+		  
+		  static void q6() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+		  static void q7() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+		  static void q8() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+		  static void q9() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+		  static void q10() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+		  static void q11() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+		  static void q12() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+		  static void q13() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+		  static void q14() {
+			    int num = 10;
+			    System.out.println(num);
+			  }
+		  
+}
